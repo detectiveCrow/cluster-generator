29a55ad03448a176733649eeed1106602119a466
detectivecrow2540 <detectivecrow2540@gmail.com>
2022-08-25 20:22:04 +0900

    CG-1 fix: fix join-command error

--- simple-ec2-cluster/destroy-instance.sh
+++ simple-ec2-cluster/destroy-instance.sh
@@ -4,10 +4,15 @@ cd ./terraform
 # ssh key variable설정
 export TF_VAR_public_key=test
 
-# key 삭제
+# 임시파일 삭제
 rm -rf key key.pub
-
 rm -rf kube_hosts
 
 # 생성한 인스턴스 삭제
 terraform destroy -auto-approve
+
+# 디렉토리 변경
+cd ../ansible
+
+# 임시파일 삭제
+rm -rf join-command
